f97daf1b5dde86ba1b142953f07490eab145bca4
Chester Asuncion <asuncionmcv@gmail.com>
2022-01-24 20:59:58 +0800

    change authenticate_user! redirect from login to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if:  :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_registration_path
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
