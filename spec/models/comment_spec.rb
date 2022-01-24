a66b3bdd8f2429ed936ccb790b7ddd3e935a2197
Chester Asuncion <asuncionmcv@gmail.com>
2022-01-24 20:23:11 +0800

    add validations with passed tests for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = User.create(
+      username: 'Re Peter',
+      email: 'ch@gmail.com',
+      password: '55treeAnts',
+      reset_password_token: 'rabbit',
+      reset_password_sent_at: DateTime.now,
+      remember_created_at: DateTime.now
+    )
+
+    user.posts.create(content: 'rabbits are best animal')
+  end
+
+  describe 'Validations' do
+    subject {
+      User.all[0].comments.create(
+        post_id: Post.all[0].id,
+        content: 'rabbits are indeed good')
+    }
+
+    it 'should be valid with the correct attributes' do
+     expect(subject).to be_valid
+    end
+
+    it 'should not be valid when it has no content' do
+     subject.content = nil
+     expect(subject).to_not be_valid
+    end
+  end
 end
