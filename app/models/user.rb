1a30a716c00a043e821d2c1566368c212165010b
Chester Asuncion <asuncionmcv@gmail.com>
2022-01-27 14:36:59 +0800

    add relations to user
    add username input in new registrations
    
    moved lines in routes
    
    migrate FriendRequests table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Associations
-  has_many :friends
   has_many :posts
   has_many :comments
   # has_many :messages
-  # has_many :friends
+
+  has_many :friends
+  has_many :sent_friend_requests, foreign_key: 'sender_id', class_name: 'FriendRequest'
+  has_many :received_friend_requests, foreign_key: 'recipient_id', class_name: 'FriendRequest'
 
   # Validations
   validates :username, :email, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_065754) do
+ActiveRecord::Schema.define(version: 2022_01_26_065412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2022_01_24_065754) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
