cf6b4cdb31482559c82242d990436e272f16f5d2
Chester Asuncion <asuncionmcv@gmail.com>
2022-01-28 19:43:13 +0800

    add association with other users as friends using UserConnection as through table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,30 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  "has_many :messages"
+  # has_many :messages
 
   has_many :sent_friend_requests, -> { where status: :pending },
            foreign_key: 'initiator_id', class_name: 'UserConnection'
   has_many :received_friend_requests, -> { where status: :pending },
            foreign_key: 'recipient_id', class_name: 'UserConnection'
-
   # rejecting pending UserConnections deletes the record
+
+  # returns UserConnection record
   has_many :initiated_friendships, -> { where status: :accepted },
            foreign_key: 'initiator_id', class_name: 'UserConnection'
   has_many :accepted_friendships, -> { where status: :accepted },
            foreign_key: 'recipient_id', class_name: 'UserConnection'
 
+  # returns User record
+  has_many :added_friends, as: :recipient_id, -> { where status: :accepted }, through: :user_connections,
+           foreign_key: :initiator_id
+
+  has_many :accepted_friends, as: :initiator_id, -> { where status: :accepted }, through: :user_connections, 
+           foreign_key: :recipient_id
+
+
   # Validations
   validates :username, :email, presence: true
+
+
 end
