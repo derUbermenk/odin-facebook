ccac372023ba2baa04cf92b684f06259b563c40c
Chester Asuncion <asuncionmcv@gmail.com>
2022-01-31 09:44:42 +0800

    add class methods, request_connection and accpet_connection for UserConnection model

--- app/models/user_connection.rb
+++ app/models/user_connection.rb
@@ -3,6 +3,25 @@ class UserConnection < ApplicationRecord
   belongs_to :initiator, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
 
+  # creates a connection
+  def self.request_connection(initiator, recipient)
+    UserConnection.create(
+      initiator: initiator,
+      recipient: recipient,
+      status: :pending
+    )
+  end
+
+  # turns a connection to an accepted_connection
+  def self.accept_connection(initiator, recipient)
+    connection = UserConnection.find_by(
+      initiator: initiator, recipient: recipient, status: :pending
+    )
+
+    connection.status = :accepted
+    connection.save
+  end
+
   # can be used both for pending and accepted connections to 
   #   reject a friend request or unfriend a user
   def self.delete_connection(user1, user2)
