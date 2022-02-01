b4da956becb4496afb2ec2389f3261616cdc4146
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-01 19:46:58 +0800

    remove Post share, like and tag models, will be creating in feature branches instead

--- db/migrate/20220131024453_fix_timestamps_for_user_connections.rb
+++
@@ -1,8 +0,0 @@
-class FixTimestampsForUserConnections < ActiveRecord::Migration[6.1]
-  def change
-    change_table :user_connections do |t|
-      t.rename :created_at, :sent_at
-      t.rename :updated_at, :accepted_at
-    end
-  end
-end
