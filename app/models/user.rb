c54405d2d45fbd2975d93da7ae2a991c8db6959f
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-01 21:38:45 +0800

    rename User and Post association to author

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :post_likes, foreign_key: 'liker_id'
   has_many :liked_posts, through: :post_likes, source: :post 
