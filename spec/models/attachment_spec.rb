656a325da298fd87efef3fc4ef33a806b7ea2fd6
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-04 19:00:32 +0800

    add test for presence of attachable_* columns in attachments table

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe Attachment, type: :model do
   describe 'Associations' do
     it { should belong_to(:post) }
     it { should belong_to(:attachable) }
+
+    it { should have_db_column(:attachable_id) }
+    it { should have_db_column(:attachable_type) }
   end
 end
