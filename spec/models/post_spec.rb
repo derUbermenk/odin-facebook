f0a63169d5e033d9bfbb3d0e573775fe3c6a7cd0
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-05 11:52:37 +0800

    add new validation specs with association with Attachment for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Post, type: :model do
 
     # for sharing
     it { should have_many(:attachments) }
+    it { should have_db_column(:attaches_count).of_type(:integer) }
     it { should have_db_column(:attachments_count).of_type(:integer) }
   end
 
@@ -23,27 +24,52 @@ RSpec.describe Post, type: :model do
         reset_password_sent_at: DateTime.now,
         remember_created_at: DateTime.now
       )
+
+      @shareable_post = User.first.posts.create(content: 'share this')
     end
 
     subject {
-      User.all[0].posts.create(
-        content: 'good rabbits'
-      )
+      User.first.posts.create(content: 'something')
     }
+
 
     it 'is valid with the correct attributes' do
       expect(subject).to be_valid
     end
-
-    it 'is invalid when there is no content' do
-      subject.content = nil
-      expect(subject).to_not be_valid
-    end
-
+    
     it 'is invalid when the content is more than 500 letters' do
       subject.content = Array.new(550, 'words').join(' ')
       expect(subject).to_not be_valid
     end
+    
+    context 'for content and attachments' do
+      it 'is invalid when there is no content and attachments' do
+        subject.content = nil
+        subject.attachments = Attachment.none
+
+        expect(subject).to_not be_valid
+      end
+
+      it 'is valid when there is content even with no attachment' do
+        subject.content = 'something'
+        expect(subject).to be_valid
+      end
+
+      it 'is valid when there are attachments even with no content' do
+        subject.content = nil
+        subject.attachments.create(attachable: @shareable_post)
+
+        expect(subject).to be_valid
+      end
+
+      it 'is valid when there are both attachments and content' do
+        subject.content = 'something'
+        subject.attachments.create(attachable: @shareable_post)
+
+        expect(subject).to be_valid
+      end
+    end
+
   end
 
   describe 'Shares' do
