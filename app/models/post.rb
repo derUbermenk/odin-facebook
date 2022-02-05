3c0d111b35494ee099b54f49d4a62a97f3b2343d
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-05 11:07:41 +0800

    rename new association names with attachments in PPost model ;

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes, class_name: 'PostLike', counter_cache: :likes_count
 
-  # shares
+  # shares i.e. attachments, referring to the act of attaching
   has_many :attachments, as: :attachable
   alias_attribute :attachments, :shares
   alias_attribute :attachments_count, :shares_count
+
+  # attached_objects actual attachements
+  has_many :attached_objects, foreign_key: 'post_id', class_name: 'Attachment'
 
   validates :content, presence: true,
                       length: {
