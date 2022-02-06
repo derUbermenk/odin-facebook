05d73bc69e27b07a312f552a3fea192b58ee5a43
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-06 15:56:02 +0800

    add faker gem and seed test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# create default_user
+User.create(
+  username: 'Default User',
+  email: 'default@password.com',
+  password: 'default_password'
+)
+
+42.times do
+  user_name = Faker::Name.unique.name
+  User.create(
+    username: user_name,
+    email: Faker::Internet.unique.email(name: user_name),
+    password: 'haketh' 
+  )
+end
+
