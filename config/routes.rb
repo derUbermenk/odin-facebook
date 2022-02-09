bdac927afca35ca5f67fc7ed6417a95d8d6e2aa3
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-09 10:56:07 +0800

    add post_liking route in posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    member do
+      post 'toggle_like', to: 'posts#toggle_like'
+    end
+  end
 
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
