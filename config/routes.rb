0b29f6e1aa7409e9d9e2c2cc13857afd597c860e
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-11 20:39:04 +0800

    add link to friends in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
       post 'share', to: 'posts#share'
     end
   end
-  resources :comments 
+  resources :comments
+  resources :users
 
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
