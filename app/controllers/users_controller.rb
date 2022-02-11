37a1333ce3ab4c37f5ce8b2643df0fd1a8363f57
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-11 22:06:01 +0800

    add friends views in index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @friends = current_user.friendships
+    @friends = current_user.friends
     @sent_requests = current_user.sent_friend_requests
     @friend_suggestions = current_user.suggested_users
   end
