0949e3b4fb3bb3f786f50d675501f8c64569b258
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-12 15:45:14 +0800

    add routes to user_connections_controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,17 @@ Rails.application.routes.draw do
   resources :comments
   resources :users
 
-  post 'add_friend/:id', to: 'user_connections#create', as: 'add_friend'
+  resources :user_connections do
+    collection do
+      post 'add/:user_id', to: 'user_connections#create', as: :add
+    end
+
+    member do
+      patch '/', to: 'user_connections#update', as: :accept
+      delete '/reject', to: 'user_connections#destroy', as: :reject
+      delete '/unfriend', to: 'user_connections#destroy', as: :delete
+    end
+  end
 
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
