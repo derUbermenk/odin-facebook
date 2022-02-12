86aad278270130fa4659f5f59df99b758fcfdc5e
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-12 11:10:30 +0800

    add view and controller action for showing user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
     @sent_requests = current_user.sent_friend_requests
     @friend_suggestions = current_user.suggested_users
   end
+
+  def show
+    @user = User.find(params[:id])
+    @user_friends_count = @user.friends.count
+    @mutual_friends_count = @user.mutual_friends(current_user).count
+    @posts = Post.where(author: @user)
+  end
 end
