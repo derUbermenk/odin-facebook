e77534d4245eadb40c015d2f28fe3acfe91645c2
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-13 15:48:20 +0800

    fix users controller to supply needed variables in index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @friends = current_user.friends
-    @sent_requests = current_user.sent_friend_requests
-    @friend_suggestions = current_user.suggested_users
+    @received_requests = current_user.received_friend_requests
+    @suggestions = current_user.suggested_users
   end
 
   def show
