1641584a1d4f470558abbf18057f321e4f95383d
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-13 09:08:17 +0800

    add refresh on add functionality in _connection_interfacing

--- app/controllers/user_connections_controller.rb
+++ app/controllers/user_connections_controller.rb
@@ -1,24 +1,35 @@
 class UserConnectionsController < ApplicationController
-  before_action :set_connection, except: %i[index create] 
+  before_action :set_connection, except: %i[index create]
   before_action :authenticate_user!
 
+  
+  # render inline: "location.reload();" reloads the page
+  #   as a response to the request
   def create
     recipient = User.find(params[:user_id])
     @request = current_user.sent_friend_requests.build recipient: recipient
     @request.save
 
     respond_to do |format|
-      format.js{}
+      format.js {render inline: "location.reload();" }
     end
   end
 
   def update
     @connection.status = :accepted
     @connection.save
+
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
   end
 
   def destroy
     @connection.destroy
+
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
   end
 
   private
