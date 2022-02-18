d3279ac9ce0f3427a0a15fe97dbe1488de10f21e
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-18 22:05:26 +0800

    allow attachable_id to be null
    add migration for allowing null attachable_id
      this is simulate deleted attachments
    
    record changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_125341) do
+ActiveRecord::Schema.define(version: 2022_02_18_131650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_125341) do
   create_table "attachments", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.string "attachable_type", null: false
-    t.bigint "attachable_id", null: false
+    t.bigint "attachable_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["attachable_type", "attachable_id"], name: "index_attachments_on_attachable"
