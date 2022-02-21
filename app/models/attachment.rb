e2b707fe27770ff63037dbc0babc8c9606b34ff7
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-21 15:24:55 +0800

    make attachable optional for Attachment model
    this handles the cases wherein the attachable has been deleted

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,4 @@
 class Attachment < ApplicationRecord
   belongs_to :post, counter_cache: :attachments_count
-  belongs_to :attachable, polymorphic: true, counter_cache: :attaches_count
+  belongs_to :attachable, polymorphic: true, counter_cache: :attaches_count, optional: true
 end
