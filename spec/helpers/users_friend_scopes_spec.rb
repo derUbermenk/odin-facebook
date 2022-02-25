0e6476029aa5695329c3fa8ed725bc77de15b980
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-25 13:56:44 +0800

    add UsersFriendScopes
    module returns a lambda for querying for a users friends
      in a bidirectional manner

--- spec/helpers/users_friend_scopes_spec.rb
+++ spec/helpers/users_friend_scopes_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 # test connection scopes for user
-RSpec.describe ConnectionScopes do
+RSpec.describe UsersFriendScopes do
 
+  # current_user does create :user only when called
   let(:current_user) { create :user }
 
   before do
@@ -12,6 +13,8 @@ RSpec.describe ConnectionScopes do
     end
 
     # make first 2 users have accepted connections
+    # current_user is only created here, therefore
+    #   it is last entry in User.all
     User.all[..1].each.with_index do |user, index|
       initiator = index.odd? ? user : current_user
       recipient = index.odd? ? current_user : user
@@ -28,25 +31,11 @@ RSpec.describe ConnectionScopes do
 
     # let 4th user have no connection
   end
-
-  describe '#connections' do
-    it 'returns all the UserConnections of user' do
-      user_conn = UserConnection.all
-      expect(current_user.connections).to contain_exactly(user_conn[0], user_conn[1], user_conn[2])
-    end
-  end
-
-  describe '#accepted_connections' do
-    it 'returns all the UserConnections with accepted status' do
-      user_conn = UserConnection.all
-      expect(current_user.accepted_connections).to contain_exactly(user_conn[0], user_conn[1])
-    end
-  end
-
-  describe '#pending_connections' do
-    it 'returns all the UserConnections with pending status' do
-      user_conn = UserConnection.all
-      expect(current_user.pending_connections).to contain_exactly(user_conn[2])
+  
+  describe '#friends' do 
+    it 'returns all users for which current_user is friends' do
+      users = User.all
+      expect(current_user.friends).to contain_exactly(users[0], users[1])
     end
   end
 end
