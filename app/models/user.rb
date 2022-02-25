39ff2128efcdcf905ee7bffd3f89ebd8fc418b95
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-25 14:16:42 +0800

    refactor user model
    moved other logic to other modules and classes
    added new custom associatons for bi directional connections
    rewrite specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# Table name: users
+#   string "username", default: "", null: false
+#   string "email", default: "", null: false
+#   string "encrypted_password", default: "", null: false
+#   string "reset_password_token"
+#   datetime "reset_password_sent_at"
+#   datetime "remember_created_at"
+#   datetime "created_at", precision: 6, null: false
+#   datetime "updated_at", precision: 6, null: false
+#   index ["email"], name: "index_users_on_email", unique: true
+#   index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,28 +17,36 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # posts related asssociations
   has_many :posts, foreign_key: 'author_id'
-  has_many :post_likes, foreign_key: 'liker_id', inverse_of: 'liker', class_name: 'PostLike'
+  has_many :post_likes, foreign_key: 'liker_id', 
+           inverse_of: 'liker', class_name: 'PostLike'
   has_many :liked_posts, through: :post_likes, source: :post
   has_many :comments
 
-  # has_many :messages
-  has_many :sent_friend_requests, UsersHelper::FriendRequest.requests(:recipient),
-           foreign_key: 'initiator_id', class_name: 'UserConnection'
-  has_many :received_friend_requests, UsersHelper::FriendRequest.requests(:initiator),
-           foreign_key: 'recipient_id', class_name: 'UserConnection'
-  has_many :user_connections
+  # one directional user_connections associations
+  has_many :initiated_connections, foreign_key: 'initiator_id', class_name: 'UserConnection'
+  has_many :received_connections, foreign_key: 'recipient_id', class_name: 'UserConnection'
+  has_many :received_friend_requests, -> { where status: :pending }, 
+           class_name: 'UserConnection', foreign_key: 'recipient_id'
+  has_many :sent_friend_requests, -> { where status: :pending},
+           class_name: 'UserConnection', foreign_key: 'initiator_id'
 
+  # bi directional user_connections associations
+  has_many :connections, UsersConnectionScopes.connections, 
+           class_name: 'UserConnection'
+  has_many :accepted_connections, UsersConnectionScopes.accepted_connections,
+           class_name: 'UserConnection'
+  has_many :pending_connections, UsersConnectionScopes.pending_connections,
+           class_name: 'UserConnection'
+
+  has_many :friends, UsersFriendScopes.friends, class_name: 'User'
+
+  # validations
   validates :username, :email, presence: true
 
   # returns User relations of mutual friends
-  def friend
-    @friend ||= Friends.new(self)
-  end
-
-  # returns User relations of mutual friends
   def mutual(other_user)
-    @mutual ||= Mutuals.new(self, other_user)
+    @mutual ||= Mutuals.new(self.friends, other_user.friends)
   end
 end
-
