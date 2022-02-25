88b3e148df8bd151544b23fa859f33fe0c50d6ff
Chester Asuncion <asuncionmcv@gmail.com>
2022-02-25 14:26:05 +0800

    refactor user_connection model and spec

--- app/models/user_connection.rb
+++ app/models/user_connection.rb
@@ -1,4 +1,31 @@
+# Table name: user_connections
+#
+#   t.bigint "author_id", null: false
+#    t.text "content"
+#    t.datetime "created_at", precision: 6, null: false
+#    t.datetime "updated_at", precision: 6, null: false
+#    t.integer "likes_count", default: 0
+#    t.integer "attachments_count", default: 0
+#    t.integer "attaches_count", default: 0
+#    t.index ["author_id"], name: "index_posts_on_author_id"
+#
 class UserConnection < ApplicationRecord
+
+  # search for the connection which involves the given users
+  # @param users [Array]
+  # @return UserConnection
+  def self.search(user1, user2)
+    UserConnection.where(
+      initiator: user1,
+      recipient: user2
+    ).or(
+      where(
+        initiator: user2,
+        recipient: user1
+      )
+    ).take || nil 
+  end
+
   enum status: [:pending, :accepted]
   belongs_to :initiator, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
@@ -9,7 +36,7 @@ class UserConnection < ApplicationRecord
 
   def no_connections_yet
     error_message = 'Connection between two users already exists'
-    UserConnection.find_with_users(initiator, recipient).nil? || errors.add(:status, error_message)
+    UserConnection.search(initiator, recipient).nil? || errors.add(:status, error_message)
   end
 
   def no_connections_with_self
@@ -17,50 +44,10 @@ class UserConnection < ApplicationRecord
     recipient == initiator && errors.add(:initiator, error_message)
   end
 
-  # creates a connection
-  def self.request_connection(initiator, recipient)
-    UserConnection.create(
-      initiator: initiator,
-      recipient: recipient,
-      status: :pending
-    ).save
-  end
-
-  # turns a connection to an accepted_connection
-  def self.accept_connection(initiator, recipient)
-    connection = UserConnection.find_by(
-      initiator: initiator, recipient: recipient, status: :pending
-    )
-
-    connection.status = :accepted
-    connection.save
-  end
-
-  # can be used both for pending and accepted connections to 
-  #   reject a friend request or unfriend a user
-  def self.delete_connection(user1, user2)
-    UserConnection.where(initiator: user1, recipient: user2)
-                  .or(UserConnection.where(initiator: user2, recipient: user1))
-                  .take
-                  .delete
-  end
-
-  # looks for the connection with the given users
-  def self.find_with_users(user1, user2)
-    where(
-      initiator: user1,
-      recipient: user2
-    ).or(
-      where(
-        initiator: user2,
-        recipient: user1
-      )
-    ).take
-  end
-
   private_class_method
 
   # rename date columns
+
   def self.timestamp_attributes_for_create
     super << 'sent_at'
   end
