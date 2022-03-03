c417f2786bcac30bc470ce61d809ed88e093f883
Chester Asuncion <asuncionmcv@gmail.com>
2022-03-03 19:51:20 +0800

    add new validations for user_connection model

--- app/models/user_connection.rb
+++ app/models/user_connection.rb
@@ -23,7 +23,7 @@ class UserConnection < ApplicationRecord
         initiator: user2,
         recipient: user1
       )
-    ).take || nil 
+    )
   end
 
   enum status: [:pending, :accepted]
@@ -31,20 +31,12 @@ class UserConnection < ApplicationRecord
   belongs_to :recipient, class_name: 'User'
 
   validates :status, :initiator, :recipient, presence: true
-  validate :no_connections_yet, on: :create
-  validate :no_connections_with_self
+  validate :connections
 
-  def no_connections_yet
-    error_message = 'Connection between two users already exists'
-    UserConnection.search(initiator, recipient).nil? || errors.add(:status, error_message)
+  def connections
+    validator = UserConnectionCustomValidator.new self
+    validator.validate
   end
-
-  def no_connections_with_self
-    error_message = "Can't establish a connection with self"
-    recipient == initiator && errors.add(:initiator, error_message)
-  end
-
-  private_class_method
 
   # rename date columns
 
@@ -54,5 +46,33 @@ class UserConnection < ApplicationRecord
 
   def self.timestamp_attributes_for_update
     super << 'accepted_at'
+  end
+
+  private_class_method :timestamp_attributes_for_create, :timestamp_attributes_for_update
+end
+
+# handles custom validations for UserConnection model
+class UserConnectionCustomValidator
+  def initialize(connection)
+    @connection = connection
+  end
+
+  def validate
+    not_connecting_with_self
+    not_duplicating_connection_with_user
+  end
+
+  private
+
+  def not_duplicating_connection_with_user
+    error_message = 'Connection between two users already exists'
+    UserConnection.search(@connection.initiator, @connection.recipient).none? ||
+      @connection.errors.add(:status, error_message)
+  end
+
+  def not_connecting_with_self 
+    error_message = "Can't establish a connection with self"
+    @connection.recipient == @connection.initiator &&
+      @connection.errors.add(:initiator, error_message)
   end
 end
